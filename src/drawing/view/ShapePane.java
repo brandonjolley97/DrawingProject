f6b69147404dd190212d4051e237720407ec47cf
brandonjolley97 <bjol7457@740s-pl-132.local>
2016-04-22 09:53:13 -0600

    started the shapePane class

--- src/drawing/view/ShapePane.java
+++ src/drawing/view/ShapePane.java
@@ -1,18 +1,22 @@
 package drawing.view;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 import javax.swing.JPanel;
 
 public class ShapePane extends JPanel
 {
+	private ArrayList<Rectangle> rectangleList;
+	
 	public ShapePane()
 	{
-		
+		rectangleList = new ArrayList<Rectangle>();
 	}
 	
 	public void addRectangle()
 	{
-		
+		rectangleList.add(new Rectangle(10, 20, 50, 10));
 	}
 
 	protected void paintComponent(Graphics currentGraphics)
